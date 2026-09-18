781ec158642a45120348b7aed45418dcf2890e0a
Kavyaa <59550242+kavyaas110@users.noreply.github.com>
2026-09-18 00:22:17 -0400

    Add: maximum-subarray - submission-5

---
+++ Data Structures & Algorithms/maximum-subarray/submission-5.py
@@ -0,0 +1,13 @@
+class Solution:
+    def maxSubArray(self, nums: List[int]) -> int:
+        overall_max = nums[0]
+        curr_max = nums[0]
+        for num in nums[1:]:
+            curr_max = max(curr_max + num,num)
+            overall_max = max(overall_max,curr_max)
+        
+        return overall_max
+
+
+
+        
